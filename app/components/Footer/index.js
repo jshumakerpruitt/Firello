41c94f9fee6fad15cce4882c031d33e1af1b47b6
Jasper <jshumakerpruitt@gmail.com>
2016-12-29 17:24:14 +0700

    remove crap/sticky footer

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Flex } from 'reflexbox'
 
-import A from 'components/A';
-import LocaleToggle from 'containers/LocaleToggle';
-import Wrapper from './Wrapper';
 import messages from './messages';
 
 function Footer() {
   return (
-    <Wrapper>
-      <section>
-        <FormattedMessage {...messages.licenseMessage} />
-      </section>
-      <section>
-        <LocaleToggle />
-      </section>
-      <section>
-        <FormattedMessage
-          {...messages.authorMessage}
-          values={{
-            author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
-          }}
-        />
-      </section>
-    </Wrapper>
+    <Flex
+      justify='center'
+    >
+      this is the footer
+    </Flex>
   );
 }
 
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,41 +1,25 @@
-/**
- *
- * App.react.js
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
 import React from 'react';
 import Helmet from 'react-helmet';
-import styled from 'styled-components';
+import { Flex } from 'reflexbox'
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
-const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
-`;
-
 function App(props) {
   return (
-    <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-        meta={[
-          { name: 'description', content: 'A React.js Boilerplate application' },
-        ]}
-      />
+    <Flex
+      style={{height: '100vh'}}
+      flexColumn
+      align='center'
+    >
       <Header />
-      {React.Children.toArray(props.children)}
+      <Flex
+        flexAuto
+      >
+        {React.Children.toArray(props.children)}
+      </Flex>
       <Footer />
-    </AppWrapper>
+    </Flex>
   );
 }
 
